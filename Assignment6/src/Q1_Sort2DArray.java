d1a239e35e113a7e0bf814c72476f1cb10d167c7
williampol <103951515+williampol@users.noreply.github.com>
2023-11-05 01:20:12 -0400

    Added Assignment6

---
+++ Assignment6/src/Q1_Sort2DArray.java
@@ -0,0 +1,91 @@
+/* Assignment 6 (100 marks in total; 5% of the final score of this course)
+ *
+ * Question 1 (20 marks)
+ *
+ * Write a java program, use "subroutine" to finish sorting a 2-dimension array.
+ *
+ * User input a 2-dimension array of double type, return it in non-decreasing
+ * order according to values in the first raw, if there are two or more same
+ * element in the first raw, keep the original sequence.
+ *
+ * INPUT: a user input 2-dimension array.
+ * OUTPUT: sorted input array in non-decreasing order according to values in the first row.
+ *
+ * Note: considering how to take users' input, you can set some rules (give users some hints)
+ * to make your code user-friendly.
+ * User will follow your instruction to input test example.
+ *
+ * Example 1
+ * INPUT: if user input array (Order 2*4) is
+ *        1.45  0.25  8.95  0.99
+ *        2.56  5.67  0.47  2.65
+ *
+ * OUTPUT: 0.25  0.99  1.45  8.95
+ *         5.67  2.65  2.56  0.47
+ *
+ * Example 2
+ * INPUT: if user input array (Order 5*5) is
+ *        0.95  0.23  0.95  0.95  0.65
+ *        2.56  5.67  0.47  2.65  0.47
+ *        1.45  0.25  8.95  0.99  0.67
+ *        5.67  2.65  0.25  8.95  0.99
+ *        1.23  4.21  0.97  7.64  8.52
+ *
+ * OUTPUT: 0.23  0.65  0.95  0.95  0.95
+ *         5.67  0.47  2.56  0.47  2.65
+ *         0.25  0.67  1.45  8.95  0.99
+ *         2.65  0.99  5.67  0.25  8.95
+ *         4.21  8.52  1.23  0.97  7.64
+ *
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q1_Sort2DArray {
+    /* place your subroutine code here */
+    static void sort2DArray(double[][] arr_to_sort) {
+        int longest_sort = 1;
+        while (longest_sort != arr_to_sort[0].length) {
+            longest_sort = 1;
+
+            for (int col = 0; col < arr_to_sort[0].length-1; col++) {
+                if (arr_to_sort[0][col] > arr_to_sort[0][col+1]) {
+                    for (int row = 0; row < arr_to_sort.length; row++) {
+                        double temp_elem = arr_to_sort[row][col+1];
+                        arr_to_sort[row][col+1] = arr_to_sort[row][col];
+                        arr_to_sort[row][col] = temp_elem;
+                    }
+                } else longest_sort++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /* place your code to run your subroutine here */
+        Scanner scanner = new Scanner(System.in);
+        double[][] array = new double[100][100];
+        int row_count = 0;
+
+        System.out.println("Enter whole row one at a time with spaces between numbers; type 'end' to end: ");
+        do {
+            String input = scanner.nextLine();
+            if (input.equals("end")) break;
+
+            String[] str_row = input.split(" ");
+            double[] doub_row = new double[str_row.length];
+            for (int i = 0; i < str_row.length; i++)
+                doub_row[i] = Double.parseDouble(str_row[i]);
+            array[row_count] = doub_row;
+
+            row_count++;
+        } while (true);
+
+        double[][] proper_array = new double[row_count][array[0].length];
+        for (int i = 0; i < proper_array.length; i++)
+            proper_array[i] = Arrays.copyOf(array[i], array[i].length);
+
+        sort2DArray(proper_array);
+        for (double[] doubles : proper_array) System.out.println(Arrays.toString(doubles));
+    }
+}
---
+++ Assignment6/src/Q2_LinkedList.java
@@ -0,0 +1,158 @@
+/* Assignment 6 (100 marks in total; 5% of the final score of this course)
+ *
+ * Question 2 (40 marks)
+    Use java to implement a data structure named LinkedList, and implement several methods of the LinkedList.
+
+ */
+
+class Node {
+    int data; // 'data' stores the value of a node.
+    Node next; // 'next' refers/points to the next node
+
+    // the construction method of class Node.
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Q2_LinkedList {
+    Node head;
+
+    // Insert 'new_data' at the beginning of the LinkedList
+    // (5 marks)
+    public void insertAtBeginning(int new_data) {
+        /* place your code here */
+        if (head == null) {
+            head = new Node(new_data);
+            return;
+        }
+
+        Node new_head = new Node(new_data);
+        new_head.next = head;
+        head = new_head;
+    }
+
+    // Insert 'new_data' at the end of the LinkedList
+    // (5 marks)
+    public void insertAtEnd(int new_data) {
+        /* place your code here */
+        if (head == null) {
+            head = new Node(new_data);
+            return;
+        }
+
+        Node head_temp = head;
+        Node insert = new Node(new_data);
+        while (head_temp.next != null) head_temp = head_temp.next;
+        head_temp.next = insert;
+    }
+
+    // Insert 'new_data' after a node referred to as 'prev_node'
+    // (10 marks)
+    public void insertAfter(Node prev_node, int new_data) {
+        /* place your code here */
+        Node insert = new Node(new_data);
+        insert.next = prev_node.next;
+        prev_node.next = insert;
+
+    }
+
+    // Delete a node located in 'position' of the Linked List. The first element of the LinkedList has a position=0.
+    // (10 marks)
+    void deleteNode(int position) {
+        /* place your code here */
+        if (position == 0) {
+            head = head.next;
+            return;
+        }
+        Node head_temp = head;
+        for (int i = 0; i < position-1; i++) head_temp = head_temp.next;
+        head_temp.next = head_temp.next.next;
+    }
+
+    // Search for a node containing the value of 'key' in the LinkedList.
+    // If there is a node in the LinkedList whose value is equal to 'key', then return 'true'.
+    // If there is no node in the LinkedList whose value is equal to 'key', then return 'false'.
+    // (15 marks)
+    boolean search(Node head, int key) {
+        /* place your code here */
+        Node head_temp = head;
+        while (head_temp.next != null) {
+            if (head_temp.data == key) return true;
+            head_temp = head_temp.next;
+        } return false;
+    }
+
+    // Sort the nodes in the LinkedList in ascending orders of their values.
+    // Requirement: please use bubble sort.
+    // Example: for a LinkedList: head->3->5->1->4->2, the sorted LinkedList should be head->1->2->3->4->5.
+    // (15 marks)
+    void sortLinkedList(Node head) {
+        /* place your code here */
+        Node head_temp;
+        boolean sorted = false;
+
+        while (!sorted) {
+            sorted = true;
+            head_temp = head;
+
+            while (head_temp.next != null) {
+                if (head_temp.data > head_temp.next.data) {
+                    Node next_temp = head_temp.next;
+                    head_temp.next = head_temp.next.next;
+                    next_temp.next = head_temp;
+                    head = next_temp;
+
+                    System.out.println(head.data);
+                    System.out.println(head.next.data);
+                    System.out.println(head.next.next.data);
+                    System.out.println(head.next.next.next.data);
+
+                    sorted = false;
+                }
+                head_temp = head_temp.next;
+            }
+        }
+    }
+
+    // Print the linked list
+    public void printList() {
+        Node tnode = head;
+        while (tnode != null) {
+            System.out.print(tnode.data + " ");
+            tnode = tnode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        /* TA may test different functions of the LinkedList here */
+        /* The following is just an example of how to do the test. */
+
+        Q2_LinkedList llist = new Q2_LinkedList();
+
+        llist.insertAtEnd(1);
+        llist.insertAtBeginning(2);
+        llist.insertAtBeginning(3);
+        llist.insertAtEnd(4);
+        llist.insertAfter(llist.head.next, 5);
+
+        System.out.println("Linked list: ");
+        llist.printList();
+
+        System.out.println("\nAfter deleting an element: ");
+        llist.deleteNode(3);
+        llist.printList();
+
+        System.out.println();
+        int item_to_find = 3;
+        if (llist.search(llist.head, item_to_find))
+            System.out.println(item_to_find + " is found");
+        else
+            System.out.println(item_to_find + " is not found");
+
+        llist.sortLinkedList(llist.head);
+        System.out.println("\nSorted List: ");
+        llist.printList();
+    }
+}
