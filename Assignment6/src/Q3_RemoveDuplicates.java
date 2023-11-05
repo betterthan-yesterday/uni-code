25e348d17ddacd0329f16e6cde969cb08310f169
williampol <103951515+williampol@users.noreply.github.com>
2023-11-05 13:49:04 -0500

    Finished sorting part of method for Q3

--- Assignment6/src/Q3_RemoveDuplicates.java
+++ Assignment6/src/Q3_RemoveDuplicates.java
@@ -26,12 +26,52 @@
  *
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q3_RemoveDuplicates {
     /* place your subroutine code here */
+    static void filter(int[] arr) {
+
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+
+            for (int i = 0; i < arr.length-1; i++) {
+                if (arr[i] < arr[i+1]) {
+                    for (int j = i+1; j < arr.length; j++) {
+                        System.out.println(Arrays.toString(arr));
+                        if (arr[j] <= arr[i]) {
+                            int temp = arr[j-1];
+                            arr[j-1] = arr[i];
+                            arr[i] = temp;
+
+                            sorted = false;
+                            break;
+                        } else if (j == arr.length-1) {
+                            int temp = arr[j];
+                            arr[j] = arr[i];
+                            arr[i] = temp;
+
+                            sorted = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         /* place your code to run your subroutine here */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the array on one line with one space between numbers: ");
+        String[] input = scanner.nextLine().split(" ");
+
+        int[] array = new int[input.length];
+        for (int i = 0; i < input.length; i++) array[i] = Integer.parseInt(input[i]);
+
+        filter(array);
+        System.out.println(Arrays.toString(array));
     }
 }
