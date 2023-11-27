bf5a893b37af95a182e22864869f2e51dd050e60
williampol <103951515+williampol@users.noreply.github.com>
2023-11-27 16:10:38 -0500

    Added Assignment 10

---
+++ Assignment10/src/Q1_TreeMapByBST.java
@@ -0,0 +1,52 @@
+/* Assignment 10 (100 marks in total; 5% of the final score of this course)
+ *
+ * Question 1 (50 marks in total)
+    Use mutils.BinarySearchTree as a basic structure to implement a TreeMap for <String, Integer>.
+    You cannot use JCF provided map.
+ */
+
+import mutils.BinarySearchTree;
+import java.util.ArrayList;
+
+public class Q1_TreeMapByBST {
+
+    // use m_bst as the basic data structure for the treemap of <String, Integer>
+    BinarySearchTree m_bst = new BinarySearchTree();
+
+    // Question 1.1 (5 marks): implement method size().
+    // Returns the number of elements in this map (its cardinality).
+    // You can ONLY modify the body of this method including the return statement.
+    public int size() {
+        return 0;
+    }
+
+    // Question 1.2 (5 marks): implement method isEmpty().
+    // Returns true if this map contains no elements, otherwise return false.
+    // You can ONLY modify the body of this method including the return statement.
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // Question 1.3 (15 marks) implement method get().
+    // If key is contained in the map, return the value of the key.
+    // If key is not contained in the map, return -1.
+    // You can ONLY modify the body of this method including the return statement.
+    public int get(String key) {
+        return -1;
+    }
+
+    // Question 1.4 (15 marks) implement method put().
+    // If the key is already contained in the map, overwrite the old value and return -1;
+    // If the key is not contained in the map, add a new <key,value> entry to the map and return 1;
+    // You can ONLY modify the body of this method including the return statement.
+    public void put(String key, int value) {
+
+    }
+
+    // Question 1.5 (10 marks) implement method getKeysInAlphabeticalOrder().
+    // return the keys of this map as an ArrayList<String> in Alphabetical order.
+    // You can ONLY modify the body of this method including the return statement.
+    public ArrayList<String> getKeysInAlphabeticalOrder(){
+        return null;
+    }
+}
---
+++ Assignment10/src/Q2_WordCounter.java
@@ -0,0 +1,46 @@
+/* Assignment 10 (100 marks in total; 5% of the final score of this course)
+ *
+ * Question 2 (50 marks in total)
+    Use the Q1_TreeMapByBST to implement a WordCounter to count words in documents.
+    You cannot use JCF provided map.
+ */
+
+public class Q2_WordCounter {
+    // use the m_treemap as the data structure for word counting
+    Q1_TreeMapByBST m_treemap = new Q1_TreeMapByBST();
+
+    // Question 2.1 (15 marks) implement buildTreeMap() to build a treemap that stores
+    // the words and word frequencies of a document into m_treemap.
+    // "filename" is the path to the file. Please use the "JingleBell.txt" under folder "data".
+    // You can ONLY modify the body of this method including the return statement.
+    public void buildTreeMap(String filename){
+
+    }
+
+    // Question 2.2 (15 marks) print all the counted words in the document in alphabetical order.
+    // You can ONLY modify the body of this method including the return statement.
+    public void printWordsAlphabetical(){
+
+    }
+
+    // Question 2.3 (15 marks) write an output file named "Output.txt" under folder "data"
+    // The output of will consist of two lists.
+    // Each list contains all the words from the file, along with the number of times
+    // that the word occurred.
+    // One list is sorted alphabetically.
+    // The other list is sorted according to the number of occurrences, with the most
+    // common words (i.e., a word with a larger frequency or count is said to be more common)
+    // at the top and the least common at the bottom.
+    // IMPORTANT: the output format is given in the file "Output_Example.txt" under folder "data".
+    // Input parameter "filename" is the path to the output file.
+    public void outputResult(String filename) {
+
+    }
+
+    // Question 2.4 (5 marks) call each of the above methods in the main() method to
+    // successfully count the words and produce expected outputs.
+    public static void main(String[] args){
+
+    }
+
+}
